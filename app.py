7d4a58857bb8cd47515a0677b7120f7aef073aef
RinQi03 <lea.qi.22@gmail.com>
2026-03-28 11:36:59 -0400

    gcloud test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World update Run!'
+    return 'Hello World update Run1111!'
 
 if __name__ == '__main__':
     # Cloud Run provides the PORT environment variable
